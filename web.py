c701e123e739ee38b3c05274f9c173dd6747b4c5
于 玺洲 <17202396@qq.com>
2015-04-06 01:14:33 +0800

    web interface

--- web.py
+++ web.py
@@ -8,63 +8,13 @@ from pymongo import MongoClient
 import pymongo
 
 
-class HomeHandler(tornado.web.RequestHandler):
-    def get(self):
-        limit = int(self.get_argument('limit', 20))
-
-        # todo autoreload
-
-        with open('target_info', 'r') as fp:
-            raw_lines = fp.readlines()
-
-        lines = []
-        for l in raw_lines[-limit:]:
-            split_line = l.split('\t')
-            if split_line[2].replace('/about', '')[8:-1] in deny:
-                continue
-            lines.append(split_line)
-        lines.reverse()
-
-        self.render('home.html', lines=lines)
-
-
-class DenyHandler(tornado.web.RequestHandler):
-    def get(self, deny_id):
-        deny.append(deny_id)
-
-        with open('deny_id', 'a') as fp:
-            fp.write('{}\n'.format(deny_id))
-        self.write('0')
-
-
-class SearchHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('search.html')
-
-    def post(self):
-        query = self.get_argument('q')
-
-        with open('target_info', 'r') as fp:
-            raw_lines = fp.readlines()
-
-        lines = []
-        for l in raw_lines:
-            split_line = l.split('\t')
-            if split_line[2].find(query) > -1:
-                lines.append(split_line)
-        lines.reverse()
-
-        self.render('home.html', lines=lines)
-
-
 class MongoHomeHandler(tornado.web.RequestHandler):
     def get(self):
         limit = int(self.get_argument('limit', 20))
 
         lines = []
         for l in self.application.c.target_info.find({}, limit=limit).sort('_id', pymongo.DESCENDING):
-            lines.append(['http://m.douban.com/group/topic/{}/'.format(l['topic_id']), l['keyword'],
-                          '/people/{}/about'.format(l['founder_id']), l['topic_title'], l['timestamp']])
+            lines.append([l['topic_id'], l['keyword'], l['founder_id'], l['topic_title'], l['timestamp']])
 
         self.render('home.html', lines=lines)
 
@@ -93,8 +43,20 @@ class MongoSearchHandler(tornado.web.RequestHandler):
         for l in self.application.c.target_info.find({
             'founder_id': query
         }).sort('_id', pymongo.DESCENDING):
-            lines.append(['http://m.douban.com/group/topic/'+l['topic_id'], l['keyword'],
-                          '/people/{}/about'.format(l['founder_id']), l['topic_title'], l['timestamp']])
+            lines.append([l['topic_id'], l['keyword'], l['founder_id'], l['topic_title'], l['timestamp']])
+
+        self.render('home.html', lines=lines)
+
+
+class MongoHistoryHandler(tornado.web.RequestHandler):
+    def get(self):
+        query = self.get_argument('q')
+
+        lines = []
+        for l in self.application.c.target_info.find({
+            'founder_id': query
+        }).sort('_id', pymongo.DESCENDING):
+            lines.append([l['topic_id'], l['keyword'], l['founder_id'], l['topic_title'], l['timestamp']])
 
         self.render('home.html', lines=lines)
 
@@ -109,6 +71,7 @@ class Application(tornado.web.Application):
         _handlers = [
             (r"/pepper", MongoHomeHandler),
             (r"/search", MongoSearchHandler),
+            (r"/history", MongoHistoryHandler),
 
             (r"/deny/(.*)", MongoDenyHandler),
         ]
@@ -116,6 +79,7 @@ class Application(tornado.web.Application):
         tornado.web.Application.__init__(self, _handlers, **settings)
 
         self.c = MongoClient('121.199.5.143').pick
+        self.c.visited_topic.ensure_index('topic_id')
 
 
 application = Application()
