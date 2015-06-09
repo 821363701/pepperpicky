3e77f1eafeaf6245f455ca3a3218b35398af5a88
yuxizhou <yuxizhou@qingtingfm.com>
2015-06-09 11:17:59 +0800

    stock

--- stock.py
+++ stock.py
@@ -15,7 +15,8 @@ STOCK_ZCJK = 'sz002657'
 
 class Stock(object):
     def __init__(self):
-        self.five_price = []
+        self.price_count = 10
+        self.price_list = []
 
     def __get_stock_price(self, stock):
         r = requests.get(TEMPLATE_URL.format(stock))
@@ -23,17 +24,35 @@ class Stock(object):
         return float(r.text.split(',')[3])
 
     def __judge_stock_price(self):
-        diff = self.five_price[0] - self.five_price[-1]
+        diff = self.price_list[0] - self.price_list[-1]
 
         forward = '+'
         if diff < 0:
             diff = -diff
             forward = '-'
 
-        rate = diff / self.five_price[0]
+        rate = diff / self.price_list[0]
         print rate
         if rate > 0.001:
             send_mail('{}{}%'.format(forward, str(rate*100)[:5]))
+
+    def __judge_by_average(self):
+        total = 0.0
+        for i in self.price_list:
+            total += i
+        avg = total / len(self.price_list)
+
+        diff = self.price_list[-1] - avg
+
+        forward = '+'
+        if diff < 0:
+            diff = -diff
+            forward = '-'
+
+        rate = diff / self.price_list[0] * 100
+        print forward + str(rate)
+        if rate > 0.1:
+            send_mail('{}{}%'.format(forward, str(rate)[:5]))
 
     def start(self):
         while True:
@@ -42,22 +61,20 @@ class Stock(object):
                 if 9 < hour < 12 or 13 < hour < 15:
                     price = self.__get_stock_price(STOCK_SH)
 
-                    print price
+                    if len(self.price_list) == self.price_count:
+                        self.price_list.pop(0)
 
-                    if len(self.five_price) == 5:
-                        self.five_price.pop(0)
-
-                    self.five_price.append(price)
-                    self.__judge_stock_price()
+                    self.price_list.append(price)
+                    self.__judge_by_average()
                 else:
-                    self.five_price = []
+                    self.price_list = []
             except Exception, e:
                 logging.error(e)
             finally:
-                time.sleep(10)
+                time.sleep(5)
 
     def test(self):
-        self.five_price = [5131.881, 5131.881, 5131.881, 5131.881, 5000.425]
+        self.price_list = [5131.881, 5131.881, 5131.881, 5131.881, 5000.425]
         self.__judge_stock_price()
 
 if __name__ == '__main__':
