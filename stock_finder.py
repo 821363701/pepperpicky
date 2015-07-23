c428df4bc5cf91f081e6def806f73c38147f5df4
于 玺洲 <17202396@qq.com>
2015-07-24 01:46:54 +0800

    stock

--- stock_finder.py
+++ stock_finder.py
@@ -16,23 +16,67 @@ __author__ = 'yu'
 '''
 
 from pymongo import MongoClient
+import requests
+
+sina_api = 'http://hq.sinajs.cn/etag.php?_=0.9219840362202376&list={}'
 
 c = MongoClient('121.199.5.143').stock
 
 stocks = c.history.distinct('stock')
 
 for stock in stocks:
-    first_open = c.history.find_one({
-        'stock': stock,
-        'date': '2015-07-09'
-    })['open']
+    days = c.history.find({
+        'stock': stock
+    })
 
-    last_close = c.history.find_one({
-        'stock': stock,
-        'date': '2015-07-22'
-    })['close']
+    high = None
+    low = None
+    now = None
 
-    rate = (last_close - first_open) / first_open * 100
+    for day in days:
+        if (not high) or (high['high'] < day['high']):
+            high = day
 
-    if rate < 10 and rate != 0.0:
-        print '{}  {}'.format(stock, rate)
+        if (not low) or (low['low'] > day['low']):
+            low = day
+
+        if day['date'] == "2015-07-22":
+            now = day
+
+    if high and low and now:
+        high_rate = (high['high'] - now['close']) / now['close'] * 100
+        low_rate = (now['close'] - low['low']) / low['low'] * 100
+
+        sline = low['low'] + (high['high'] - low['low']) / 8
+
+        if now['close'] < sline:
+            parts = stock.split('.')
+            if parts[1] == 'SS':
+                r = requests.get(sina_api.format('sh'+parts[0]))
+            else:
+                r = requests.get(sina_api.format('sz'+parts[0]))
+            name = r.text.split(',')[0].split('"')[1]
+            if r.text.split(',')[1] != '0.00':
+                print u"{} {} -- {}({})[{}] {}[{}] {}({})[{}]".format(stock, name, high['high'], high_rate, high['date'],
+                                                    now['close'], now['date'], low['low'], low_rate, low['date'])
+            r.close()
+    else:
+        print "error {}".format(stock)
+
+
+
+# for stock in stocks:
+#     first_open = c.history.find_one({
+#         'stock': stock,
+#         'date': '2015-07-09'
+#     })['open']
+#
+#     last_close = c.history.find_one({
+#         'stock': stock,
+#         'date': '2015-07-22'
+#     })['close']
+#
+#     rate = (last_close - first_open) / first_open * 100
+#
+#     if rate < 10 and rate != 0.0:
+#         print '{}  {}'.format(stock, rate)
