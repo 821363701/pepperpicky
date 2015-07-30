5eb18c2ee8393203f247e43fa015c23847fc09bc
yuxizhou <yuxizhou@qingtingfm.com>
2015-07-30 10:30:33 +0800

    stock

--- stock/stock_history.py
+++ stock/stock_history.py
@@ -124,7 +124,7 @@ def get_all():
                 continue
 
             pre, where = prefix
-            date = '2015-07-28'
+            date = '2015-07-29'
 
             get_history(pre, where, a, date, date, True)
 
