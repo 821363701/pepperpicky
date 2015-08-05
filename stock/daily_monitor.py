a84d5ad0128d727297e877fe9bd0d7107f4c7b35
yuxizhou <yuxizhou@qingtingfm.com>
2015-08-05 16:05:58 +0800

    stock

---
+++ stock/daily_monitor.py
@@ -0,0 +1,105 @@
+from __future__ import print_function
+__author__ = 'yu'
+
+import os
+clear = lambda: os.system('clear')
+from util import get_stock_price, rate
+from datetime import datetime
+
+
+class Stock:
+    def __init__(self, stock, name):
+        self.stock = stock
+        self.name = name
+        self.five_price = []
+        self.available = True
+        self.reopen = False
+
+    def get_price(self):
+        price = get_stock_price(self.stock)
+
+        # today, yesterday, current, high, low
+        today = float(price[1])
+        yesterday = float(price[2])
+        current = float(price[3])
+        high = float(price[4])
+        low = float(price[5])
+        volume = float(price[8])
+        name = price[0].split('"')[-1]
+
+        if today == '0.00':
+            return u'{}(0.00)'.format(self.stock)
+
+        r = rate(current, yesterday)
+
+        if not self.available and r < 9.9:
+            self.reopen = True
+
+        if r > 9.9:
+            self.available = False
+        else:
+            self.available = True
+
+        if len(self.five_price) == 5:
+            self.five_price.pop(0)
+        self.five_price.append(r)
+
+        if len(self.five_price) > 1:
+            forward = self.five_price[-1] - self.five_price[-2]
+        else:
+            forward = '0.00'
+
+        r = str(r)[:4]
+        forward = str(forward)[:4]
+
+        buy = int(price[10]) + int(price[12]) + int(price[14]) + int(price[16]) + int(price[18])
+        sell = int(price[20]) + int(price[22]) + int(price[24]) + int(price[26]) + int(price[28])
+
+        if sell == 0:
+            bs = float(buy) / float(1)
+            if volume > 0:
+                buy_rate = buy /volume
+        else:
+            bs = float(buy) / float(sell)
+
+        if bs > 1:
+            bs = str(bs)[:4]
+            return u'{}({} {})({})'.format(self.stock, r, forward, bs)
+        else:
+            return u''
+
+
+if __name__ == '__main__':
+    all_stock = []
+    with open('buy_rate.dat', 'r') as fp:
+        for line in fp.readlines():
+            stock, name, r, buy_rate = line.split('  ')
+            all_stock.append(Stock(stock, name))
+
+    while True:
+        result = []
+        for stock in all_stock:
+            if stock.reopen:
+                result.append(stock.get_price()+'**')
+            else:
+                if stock.available:
+                    result.append(stock.get_price())
+                else:
+                    result.append(stock.get_price()+'#')
+
+        clear()
+
+        index = 0
+        for l in result:
+            if not l:
+                continue
+
+            if (index+1) % 5 == 0:
+                print(l, end='\n')
+            else:
+                print(l, end='\t')
+
+            index += 1
+
+        print('')
+        print(datetime.now())
--- stock/intime.py
+++ stock/intime.py
@@ -4,37 +4,41 @@ __author__ = 'yu'
 from util import get_all_stock, get_stock_price, rate, get_stock_history_by_date, calc_s_line
 
 
-rank = []
-for stock in get_all_stock():
-    price = get_stock_price(stock)
+def find():
+    rank = []
+    for stock in get_all_stock():
+        price = get_stock_price(stock)
 
-    # today, yesterday, current, high, low
-    today = float(price[1])
-    yesterday = float(price[2])
-    current = float(price[3])
-    high = float(price[4])
-    low = float(price[5])
-    volume = float(price[8])
-    name = price[0].split('"')[-1]
+        # today, yesterday, current, high, low
+        today = float(price[1])
+        yesterday = float(price[2])
+        current = float(price[3])
+        high = float(price[4])
+        low = float(price[5])
+        volume = float(price[8])
+        name = price[0].split('"')[-1]
 
-    if today == '0.00':
-        continue
+        if today == '0.00':
+            continue
 
-    r = rate(current, yesterday)
+        r = rate(current, yesterday)
 
-    buy = int(price[10]) + int(price[12]) + int(price[14]) + int(price[16]) + int(price[18])
-    sell = int(price[20]) + int(price[22]) + int(price[24]) + int(price[26]) + int(price[28])
+        buy = int(price[10]) + int(price[12]) + int(price[14]) + int(price[16]) + int(price[18])
+        sell = int(price[20]) + int(price[22]) + int(price[24]) + int(price[26]) + int(price[28])
 
-    if sell == 0:
-        bs = float(buy) / float(1)
-        if volume > 0:
-            buy_rate = buy /volume
-            if buy_rate > 0.1:
-                print u'{}  {}  {}  {}'.format(stock, name, r, buy_rate)
-    else:
-        bs = float(buy) / float(sell)
+        if sell == 0:
+            bs = float(buy) / float(1)
+            if volume > 0:
+                buy_rate = buy /volume
+                if buy_rate > 0.1:
+                    print u'{}  {}  {}  {}'.format(stock, name, r, buy_rate)
+        else:
+            bs = float(buy) / float(sell)
 
 
 
-    # if bs > 10 and r < 9.9:
-    #     print u'{}  {}  {}  {}'.format(stock, name, r, bs)
+        # if bs > 10 and r < 9.9:
+        #     print u'{}  {}  {}  {}'.format(stock, name, r, bs)
+
+
+
