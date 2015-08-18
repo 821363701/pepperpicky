dbb66a867d65e4a5fb64b1dd2b952bdbfa6075d1
yuxizhou <yuxizhou@qingtingfm.com>
2015-08-18 10:17:16 +0800

    pick

--- picker_mongo.py
+++ picker_mongo.py
@@ -43,9 +43,9 @@ class Picker(object):
             'source': SOURCE_DOUBAN,
         })
 
-        # if area in [u'上海', u'四川成都', u'重庆', u'福建厦门', u'江苏南京', u'浙江杭州', u'陕西西安']:
-        if area in [u'上海', u'江苏南京']:
-            send_mail_ex('['+area+']'+title, 'http://douban.com/group/topic/'+topic_id)
+        # # if area in [u'上海', u'四川成都', u'重庆', u'福建厦门', u'江苏南京', u'浙江杭州', u'陕西西安']:
+        # if area in [u'上海', u'江苏南京']:
+        #     send_mail_ex('['+area+']'+title, 'http://douban.com/group/topic/'+topic_id)
 
     def __append_visited_topic(self, url, title):
         topic_id = url.split('/')[-2]
