4c73447772ed92a0d22c67146e65b35e029f3ece
yuxizhou <yuxizhou@qingtingfm.com>
2015-10-12 10:38:49 +0800

    pick

--- stock/stock_rong_to_db.py
+++ stock/stock_rong_to_db.py
@@ -9,6 +9,11 @@ if __name__ == '__main__':
     for stock in get_all_stock():
         if stock.startswith('000') or stock.startswith('002'):
             day = datetime.now()+timedelta(days=-1)
+            if day.weekday() == 6:
+                day = datetime.now()+timedelta(days=-3)
+            elif day.weekday() == 5:
+                day = datetime.now()+timedelta(days=-2)
+
             date = day.strftime('%Y-%m-%d')
             result = get_rong_sz(stock[:6], date)
             if result:
