d5e780dc16c4a611684e4c326e64cde1304abc84
yuxizhou <yuxizhou@qingtingfm.com>
2015-10-15 16:52:20 +0800

    pick

--- stock/stock_rong_history_to_db.py
+++ stock/stock_rong_history_to_db.py
@@ -4,6 +4,27 @@ from datetime import datetime, timedelta
 from rong import get_rong_sz
 from util import get_all_stock, c
 
+'''
+002024.SZ
+002028.SZ
+002050.SZ
+002108.SZ
+002223.SZ
+002237.SZ
+002242.SZ
+002261.SZ
+002262.SZ
+002266.SZ
+002269.SZ
+002375.SZ
+002393.SZ
+002414.SZ
+002416.SZ
+002475.SZ
+002476.SZ
+002594.SZ
+'''
+
 
 def to_db(stock, date, result):
     code, name, rongzi_buy, rongzi_balance, rongquan_sell_liang, rongquan_balance_liang, rongquan_balance, rong_all_balance = result
