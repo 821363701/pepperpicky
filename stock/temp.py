ff7bb83a3273efa96d2402f8e33d878cb70ee620
yuxizhou <yuxizhou@qingtingfm.com>
2015-10-16 15:12:47 +0800

    pick

--- stock/temp.py
+++ stock/temp.py
@@ -1,19 +1,42 @@
 __author__ = 'yu'
 
-from util import get_all_stock, get_stock_history_by_date, c
+from util import get_all_stock, get_stock_history_by_date, c, rate
+
+'''
+002016.SZ
+002065.SZ
+002078.SZ
+002095.SZ
+002142.SZ
+002151.SZ
+002154.SZ
+002311.SZ
+002378.SZ
+002385.SZ
+002407.SZ
+002410.SZ
+002429.SZ
+002482.SZ
+'''
+
+
+def i(s):
+    return float(s.replace(',', ''))
 
 
 def scan(stock):
-    pot1 = get_stock_history_by_date(stock, '2015-10-08')
-    pot2 = get_stock_history_by_date(stock, '2015-10-09')
-    pot3 = get_stock_history_by_date(stock, '2015-10-12')
+    # pot1 = get_stock_history_by_date(stock, '2015-10-08')
+    # pot2 = get_stock_history_by_date(stock, '2015-10-09')
+    # pot3 = get_stock_history_by_date(stock, '2015-10-12')
     pot4 = get_stock_history_by_date(stock, '2015-10-13')
     pot5 = get_stock_history_by_date(stock, '2015-10-14')
+    pot6 = get_stock_history_by_date(stock, '2015-10-15')
 
-    if pot1 and pot2 and pot3 and pot4 and pot5:
-        if 'rong_all_balance' in pot1:
-            if pot1['rong_all_balance'] < pot2['rong_all_balance'] < pot3['rong_all_balance'] < pot4['rong_all_balance'] < pot5['rong_all_balance']:
-                print stock
+    if pot4 and pot5 and pot6:
+        if 'rong_all_balance' in pot6 and pot6['close'] != 0.0:
+            if i(pot4['rong_all_balance']) > i(pot5['rong_all_balance']) > i(pot6['rong_all_balance']):
+                r = str(rate(i(pot4['rong_all_balance']), i(pot6['rong_all_balance'])))[:5]
+                print '{} {}'.format(stock, r)
 
 
 if __name__ == '__main__':
